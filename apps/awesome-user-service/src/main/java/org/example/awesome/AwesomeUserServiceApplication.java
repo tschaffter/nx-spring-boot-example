b60f3f6984e3321169828229e337ffaf59e386a4
tschaffter <thomas.schaffter@gmail.com>
2022-06-28 15:55:39 +0000

    Import lib into app

--- apps/awesome-user-service/src/main/java/org/example/awesome/AwesomeUserServiceApplication.java
+++ apps/awesome-user-service/src/main/java/org/example/awesome/AwesomeUserServiceApplication.java
@@ -2,11 +2,14 @@ package org.example.awesome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.example.awesome.model.dto.UserStatus;
 
 @SpringBootApplication
 public class AwesomeUserServiceApplication {
 
 	public static void main(String[] args) {
+    UserStatus status;
+
 		SpringApplication.run(AwesomeUserServiceApplication.class, args);
 	}
 
