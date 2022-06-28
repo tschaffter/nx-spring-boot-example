8b41f6f39b4f7551c8b769c794b953b3534e10d4
tschaffter <thomas.schaffter@gmail.com>
2022-06-28 15:40:52 +0000

    Add user service

---
+++ apps/awesome-user-service/src/main/java/org/example/awesome/AwesomeUserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.awesome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeUserServiceApplication.class, args);
+	}
+
+}
