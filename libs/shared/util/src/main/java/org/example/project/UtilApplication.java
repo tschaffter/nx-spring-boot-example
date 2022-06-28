9d81dbce8908ba4f2b57831de02cb44e26daf93b
tschaffter <thomas.schaffter@gmail.com>
2022-06-28 14:57:21 +0000

    Create shared lib

---
+++ libs/shared/util/src/main/java/org/example/project/UtilApplication.java
@@ -0,0 +1,13 @@
+package org.example.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilApplication.class, args);
+	}
+
+}
